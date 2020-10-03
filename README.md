013c191e36de7e6277a28ff407ba04c85dd5b675
Yash <yashshirbhate@pm.me>
2020-10-03 11:58:25 +0530

    made changes as per guidelines

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 <br>
 Input data[Check sheet file]:
 ```
-A | S.Y | A | 20 | 14 | 22
-B | T.Y | B | 22 | 24 | 21
-C | F.Y | B | 19 | 4 | 20
-D | T.Y | A | 5 | 4 | 9
-E | T.Y | B | 20 | 18 | 25
+Manish | S.Y | A | 20 | 14 | 22 | 10 | 18 
+Bhavesh | T.Y | B | 22 | 24 | 21 | 20 | 23 
+Chaitanya | F.Y | B | 19 | 4 | 20 | 7 | 14 
+Dhairya | T.Y | A | 5 | 4 | 9 | 13 | 18 
+Emanuel | T.Y | B | 20 | 18 | 25 | 25 | 15 
 
 ```
--- awk.sh
+++ awk.sh
@@ -1,6 +1,12 @@
 #AWK Programming-OS Assignment 3
 #author: Yash Shirbhate
-awk -F "|" ' {print "Student",$1,"scored",$4+$5+$6,"total marks &",percent=(($4+$5+$6)/75)*100,"% percentile."}{if(percent<40) print "Result: Sorry, you failed. Better luck next time?\n"
-else if(percent>60&&percent<65) print "Result: First Class\n"
-else if(percent>=66) print "Result: Distinction\n"
-else print "Result: Pass\n"}' sheet
+awk -F "|" ' {OFS="|"  ; percent=(($4+$5+$6+$7+$8)/125)*100
+ if(percent<40) print NR" "," " $0 ," Total: "$4+$5+$6," " percent"% ", " Result: F\n"
+else if(percent>=40&&percent<=45) print NR" "," "$0 ," Total: "$4+$5+$6+$7+$8," " percent"% "," Result: C\n"
+else if(percent>=46&&percent<=55) print NR" "," "$0 ," Total: "$4+$5+$6+$7+$8," " percent"% "," Result: C+\n"
+else if(percent>=56&&percent<=65) print NR" "," "$0 ," Total: "$4+$5+$6+$7+$8," " percent"% "," Result: B\n"
+else if(percent>=66&&percent<=75) print NR" "," "$0 ," Total: "$4+$5+$6+$7+$8," " percent"% "," Result: B+\n"
+else if(percent>=76&&percent<=85) print NR" "," "$0 ," Total: "$4+$5+$6+$7+$8," " percent"% "," Result: A\n"
+else if(percent>=86&&percent<=95) print NR" "," "$0 ," Total: "$4+$5+$6+$7+$8," " percent"% "," Result: A+\n"
+else if(percent>95) print NR" "," "$0 ," Total: "$4+$5+$6," " percent"%  "," Result: O\n"
+}' sheet
